05e9216ee8956ed9a2a7303e6bb5798278ea9f35
Marko Rodic <mrmarkorodic@googlemail.com>
2017-08-01 23:07:32 +0100

    add ball class

--- js/script.js
+++ js/script.js
@@ -6,18 +6,22 @@ class Game {
         this.ctx = this.canvas.getContext("2d");
         this.gameSize = { x: this.canvas.width, y: this.canvas.height }
         this.player = new Player(this.gameSize)
+        this.ball = new Ball(this.gameSize)
         this.lives = 3
         this.score = 0
     }
 
     draw() {
-        this.drawRect(this.player)
+        this.drawRect(this.player, this.ball)
     }
 
-    drawRect(body) {
-        this.ctx.fillRect(body.center.x - body.size.x / 2,
-            body.center.y - body.size.y / 2,
-            body.size.x, body.size.y)
+    drawRect(player, ball) {
+        this.ctx.fillRect(player.center.x - player.size.x / 2,
+            player.center.y - player.size.y / 2,
+            player.size.x, player.size.y)
+        this.ctx.fillRect(ball.center.x - ball.size.x / 2,
+            ball.center.y - ball.size.y / 2,
+            ball.size.x, ball.size.y)
     }
 
     playGame() {
@@ -29,6 +33,14 @@ class Player {
     constructor(gameSize) {
         this.size = { x: 100, y: 10 }
         this.center = { x: gameSize.x / 2, y: gameSize.y-2 }
+    }
+}
+
+class Ball {
+    constructor(gameSize) {
+        this.gameSize = gameSize
+        this.size = { x: 6, y: 6 }
+        this.center = { x: 250, y: 450 }
     }
 }
 
