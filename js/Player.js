7673eb4d6a602fd7bcea7bde80f93744b1ab9d01
markorodic <mrmarkorodic@googlemail.com>
2018-01-31 22:48:39 +0000

    Stop paddle from moving out of game bounds
    The paddle is able to move beyond the game bounds. This lowers the game experience and creates a number of issues for the player. Check whether the paddle is about to leave the game area and adjust it's position accordingly.

--- js/Player.js
+++ js/Player.js
@@ -8,9 +8,17 @@ class Player {
     }
     update() {
         if (this.input.keyboardPress(this.input.key.left)) {
-            this.center.x -= 4
+            if (this.center.x < 47) {
+                this.center.x
+            } else {
+                this.center.x -= 4
+            }
         } else if (this.input.keyboardPress(this.input.key.right)) {
-            this.center.x += 4
+            if (this.center.x > 447) {
+                this.center.x
+            } else {
+                this.center.x += 4
+            }
         } else if (this.input.keyboardPress(this.input.key.space)) {
             this.space = true
         }
